f4ae647ce5d3caed57ad2afc53d908edf778649e
Sebastian Kubalski <sebastian.kubalski@gmail.com>
2018-11-30 15:15:10 +0100

    added invoices list view

--- src/app/invoices/invoices.page.ts
+++ src/app/invoices/invoices.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../services/api.service';
+import {Invoice} from '../typing/Invoice';
+import {isEmpty} from 'lodash';
 
 @Component({
   selector: 'app-invoices',
   templateUrl: './invoices.page.html',
-  styleUrls: ['./invoices.page.scss'],
+  styleUrls: ['./invoices.page.scss']
 })
 export class InvoicesPage implements OnInit {
+  public invoices: Invoice[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private readonly apiService: ApiService,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.init();
+  }
+
+  doInfiniteScroll(infiniteScroll): void {
+    if (isEmpty(this.invoices)) {
+      infiniteScroll.target.complete();
+      return;
+    }
+    const lastId = this.invoices[this.invoices.length - 1].id;
+    this.apiService
+      .getInvoices(lastId)
+      .subscribe(invoices => {
+        this.invoices.push(...invoices);
+        infiniteScroll.target.complete();
+      });
+  }
+
+  doRefresh(refresher): void {
+    this.init(refresher.target.complete);
+  }
+
+  private init(cb?: Function): void {
+    this.apiService
+      .getInvoices()
+      .subscribe(invoices => {
+        this.invoices = invoices;
+        if (cb) {
+          cb();
+        }
+      });
+  }
 }
